1307fc662347a2d17d30c1b02e16923169692e15
unknown <torsteov@stud.ntnu.no>
2023-01-06 23:07:11 +0100

    Prototype movement and input handling

--- Player.gd
+++ Player.gd
@@ -5,15 +5,16 @@ export var speed := 100.0
 var _direction := Vector3.ZERO
 var _velocity := Vector3.ZERO
 
-func _ready():
-	pass
-
 func _process(delta):
 	# Get innput
 	_direction = Vector3.ZERO
 	_direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	_direction.z = Input.get_action_strength("down") - Input.get_action_strength("up")
 	_direction = _direction.normalized()
+	
+	# Rotate model
+	# What happens when _direction == ZERO
+	look_at(global_transform.origin + _direction, Vector3.UP)
 
 func _physics_process(delta):
 	_velocity = _direction * speed * delta
