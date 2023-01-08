2811908d4b6f962e242a7a637e31e3a70fb74f71
Torstein <60714820+TorsteinOvstedal@users.noreply.github.com>
2023-01-08 16:49:21 +0100

    logic-fix

--- Environmnent/Mobs/MobA.gd
+++ Environmnent/Mobs/MobA.gd
@@ -18,7 +18,7 @@ func _process(delta):
 			pass
 		States.CHASING:
 			var p0 = global_transform.origin
-			var p1 = $Bee.global_transform.origin
+			var p1 = _target.global_transform.origin
 			var v  = (p1 - p0).normalized()
 			var angle = atan2(-v.x, -v.z)
 			rotation.y = lerp_angle(rotation.y, angle, 2 * delta)
